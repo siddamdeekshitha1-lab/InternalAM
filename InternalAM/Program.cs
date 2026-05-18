3373446a11ad6fc247474e7dae4f2b1e87136341
Siddam Deekshitha <siddamdeekshitha1@gmail.com>
2026-05-18 11:04:32 +0530

    Add project files.

---
+++ InternalAM/Program.cs
@@ -0,0 +1,50 @@
+﻿using StudentData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalAM
+{
+    /*
+     * 
+     * Internal AccessModifier=it can be accessed throughout the project
+     * 
+     */
+    class MyClass
+    {
+        int a;
+        internal MyClass()
+        {
+            a = 10;
+        }
+        public void Display()
+        {
+            Console.WriteLine("value of A is  :" + a);
+        }
+    }
+    class NewClass
+    {
+       
+
+        public void Method()
+        {
+            NewClass obj= new NewClass();
+            Console.WriteLine(obj.a);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.Display();
+            NewClass obj2= new NewClass();
+            obj2.Method();
+                Student std=new Student();
+            std.DisplayDetails();
+
+        }
+    }
+}
---
+++ StudentData/Student.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentData
+{
+    public class Student
+    {
+        int id;
+        string name;
+        internal Student()
+        {
+            id = 1;
+            name = "ABC";
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Student id is:" + id);
+            Console.WriteLine("Student name is:" + name);
+            
+        }
+    }
+}
